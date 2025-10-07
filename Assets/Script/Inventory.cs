d5bbbde02efa49e76ba94510d963c69406a18eab
zzokkss <oceanmangat@gmail.com>
2025-10-07 14:11:44 +1300

    07/10/2025

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool hasKeycardOne = false;
+    public bool hasKeycardTwo = false;
+    public bool hasKeycardThree = false;
+    public bool hasKeycardFour = false;
+}
